458265cd2234de66e0c6da99670a8d9f8cf7dfe0
tguo9 <tguo9@0a464f6a-4d89-4ce0-9171-715b3e82b72d>
2017-01-26 18:31:10 +0000

    adding working PrintDiamond.java

---
+++ Initials.java
@@ -0,0 +1,15 @@
+public class Initials {
+
+	public static void main(String[] args) {
+
+		System.out.println("T T T T T T T     G  G G G");
+		System.out.println("T T T T T T T    G  ");
+		System.out.println("    T T        G G   ");
+		System.out.println("    T T        G G   G G G");
+		System.out.println("    T T        G G       G");
+		System.out.println("    T T          G G     G");
+		System.out.println("    T T            G G   G");
+		System.out.println("    T T              G G G");
+
+	}
+}
---
+++ PrintDiamond.java
@@ -0,0 +1,17 @@
+public class PrintDiamond {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("    *");
+		System.out.println("   * *");
+		System.out.println("  * * *");
+		System.out.println(" * * * *");
+		System.out.println("  * * *");
+		System.out.println("   * *");
+		System.out.println("    *");
+
+	}
+
+	
+
+}
