aff2fb8936abd4852d7d4622c2e7088a3ebd0ad4
Tao <tguo9@dons.usfca.edu>
2017-09-18 12:01:00 -0700

    Add files via upload

---
+++ lab1/PrintDiamond.java
@@ -0,0 +1,22 @@
+/**
+	This is a program will print the diamond shape.
+	@authur Tao Guo
+**/
+
+public class PrintDiamond {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("    *");
+		System.out.println("   * *");
+		System.out.println("  * * *");
+		System.out.println(" * * * *");
+		System.out.println("  * * *");
+		System.out.println("   * *");
+		System.out.println("    *");
+
+	}
+
+	
+
+}
